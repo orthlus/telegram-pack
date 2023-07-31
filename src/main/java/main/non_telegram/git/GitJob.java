6d780eb90d545aa1470be3c42a403f29bcd61a4e
ort <orthodoxplus@ya.ru>
2023-08-01 00:14:41 +0300

    add git work

---
+++ src/main/java/main/non_telegram/git/GitJob.java
@@ -0,0 +1,131 @@
+package main.non_telegram.git;
+
+
+import lombok.extern.slf4j.Slf4j;
+import main.Main;
+import main.common.QuartzJobsList;
+import org.eclipse.jgit.api.CloneCommand;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.transport.CredentialsProvider;
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
+import org.jooq.lambda.tuple.Tuple2;
+import org.quartz.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
+
+import static java.nio.file.Files.walk;
+import static java.nio.file.Files.writeString;
+import static java.nio.file.StandardOpenOption.APPEND;
+import static java.nio.file.StandardOpenOption.TRUNCATE_EXISTING;
+import static java.time.LocalDateTime.now;
+import static main.common.QuartzUtils.buildJob;
+import static org.eclipse.jgit.api.Git.cloneRepository;
+
+@Slf4j
+@Component
+@DisallowConcurrentExecution
+public class GitJob implements Job, QuartzJobsList {
+	@Value("${app.git.login}")
+	private String login;
+	@Value("${app.git.password}")
+	private String pass;
+	@Value("${app.git.url}")
+	private String gitUrl;
+	private final String gitLocalPath = "/tmp/" + UUID.randomUUID();
+
+	@PostConstruct
+	private void init() throws IOException {
+		Files.createDirectory(Path.of(gitLocalPath));
+	}
+
+	@Override
+	public List<Tuple2<JobDetail, Trigger>> getJobs() {
+		return List.of(
+				buildJob(GitJob.class, "0 0 4 * * ?")
+		);
+	}
+
+	@Override
+	public void execute(JobExecutionContext context) throws JobExecutionException {
+		try {
+			execute();
+		} catch (GitAPIException | IOException | NoSuchElementException | InterruptedException e) {
+			log.error("Error git job", e);
+		}
+	}
+
+	private void execute() throws GitAPIException, IOException, InterruptedException {
+		CredentialsProvider cred = creds();
+
+		Path repoLocalPath = Path.of(gitLocalPath);
+		clearDir(repoLocalPath);
+
+		CloneCommand cloneCommand = cloneRepository()
+				.setURI(gitUrl)
+				.setDirectory(repoLocalPath.toFile())
+				.setCredentialsProvider(cred);
+
+		int count = new Random().nextInt(3, 35);
+
+		try (Git git = cloneCommand.call()) {
+			Path gitDir = git.getRepository().getWorkTree().toPath();
+			try (Stream<Path> textFile = walk(gitDir).filter(this::filter)) {
+				Path file = textFile.findFirst().orElseThrow();
+				for (int i = 0; i < count; i++) {
+					TimeUnit.SECONDS.sleep(1);
+					doChanges(file, git);
+				}
+			}
+
+			git.push().setCredentialsProvider(cred).call();
+		}
+	}
+
+	private void doChanges(Path file, Git git) throws IOException, GitAPIException {
+		smartChangeFile(file);
+
+		String message = "add again at " + now(Main.zone);
+		git.commit()
+				.setMessage(message)
+				.setAll(true)
+				.call();
+	}
+
+	private void smartChangeFile(Path file) throws IOException {
+		int size = Files.readAllLines(file).size();
+		StandardOpenOption option = size > 1000 ? TRUNCATE_EXISTING : APPEND;
+
+		writeString(file, "\ntext", option);
+	}
+
+	private boolean filter(Path f) {
+		return f.getFileName().toString().equals("text.txt");
+	}
+
+	private CredentialsProvider creds() {
+		return new UsernamePasswordCredentialsProvider(login, pass);
+	}
+
+	private void clearDir(Path dir) {
+		try (Stream<Path> walk = walk(dir)) {
+			walk.sorted(Comparator.reverseOrder())
+					.filter(path -> !dir.equals(path))
+					.map(Path::toFile)
+					.forEach(File::delete);
+		} catch (IOException e) {
+			log.error("Git error clean dir", e);
+			throw new RuntimeException(e);
+		}
+	}
+}
