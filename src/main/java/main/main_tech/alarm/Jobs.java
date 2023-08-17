61dd58199e8b7e66480fa1cd9ee6c54a390fff27
ort <orthodoxplus@ya.ru>
2023-08-17 10:20:32 +0300

    daily alarm refactoring and add second alarm

--- src/main/java/main/main_tech/alarm/Jobs.java
+++ src/main/java/main/main_tech/alarm/Jobs.java
@@ -1,9 +1,10 @@
 package main.main_tech.alarm;
 
+import lombok.RequiredArgsConstructor;
 import main.common.QuartzJobsList;
+import main.main_tech.Telegram;
 import org.jooq.lambda.tuple.Tuple2;
-import org.quartz.JobDetail;
-import org.quartz.Trigger;
+import org.quartz.*;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -11,12 +12,53 @@ import java.util.List;
 import static main.common.QuartzUtils.buildJob;
 
 @Component
+@RequiredArgsConstructor
 public class Jobs implements QuartzJobsList {
+	private final Telegram telegram;
+
 	@Override
 	public List<Tuple2<JobDetail, Trigger>> getJobs() {
 		return List.of(
-				buildJob(DailyJob.class, "0 29 11 ? * 2-6"),
-				buildJob(DailyCleanJob.class, "0 35 11 ? * 2-6")
+				buildJob(DailyJob1.class, "30 29 11 ? * 2-6"),
+				buildJob(DailyCleanJob1.class, "0 35 11 ? * 2-6"),
+				buildJob(DailyJob2.class, "30 29 12 ? * 2-6"),
+				buildJob(DailyCleanJob2.class, "0 35 12 ? * 2-6")
 		);
+	}
+
+	@Component
+	@DisallowConcurrentExecution
+	public class DailyJob1 implements Job {
+		@Override
+		public void execute(JobExecutionContext context) {
+			telegram.sendAlarm1("Видеочат (в маленьком чате)");
+		}
+	}
+
+	@Component
+	@DisallowConcurrentExecution
+	public class DailyCleanJob1 implements Job {
+		@Override
+		public void execute(JobExecutionContext context) {
+			telegram.deleteLastAlarmMessage1();
+		}
+	}
+
+	@Component
+	@DisallowConcurrentExecution
+	public class DailyJob2 implements Job {
+		@Override
+		public void execute(JobExecutionContext context) {
+			telegram.sendAlarm2("Голосовой чат (ссылка в общем чате)");
+		}
+	}
+
+	@Component
+	@DisallowConcurrentExecution
+	public class DailyCleanJob2 implements Job {
+		@Override
+		public void execute(JobExecutionContext context) {
+			telegram.deleteLastAlarmMessage2();
+		}
 	}
 }
