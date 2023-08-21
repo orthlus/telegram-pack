78b54522933f2f60c0d3974a3d39324d562161d8
ort <orthodoxplus@ya.ru>
2023-08-22 00:53:06 +0300

    wg service

--- src/main/java/main/main_tech/wg/WgService.java
+++ src/main/java/main/main_tech/wg/WgService.java
@@ -1,12 +1,12 @@
 package main.main_tech.wg;
 
+import java.math.BigInteger;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class WgService {
-	private record WgStatItem(String name, String up, String down, String time) {}
-
 	private Set<WgStatItem> parse(String raw) {
 		return Arrays
 				.stream(raw.split("\n"))
@@ -18,5 +18,24 @@ public class WgService {
 						cols[3]
 				))
 				.collect(Collectors.toSet());
+	}
+
+	private Set<WgStatItem> getDiffByUser(Set<WgStatItem> current, Set<WgStatItem> last) {
+		Set<WgStatItem> result = new HashSet<>();
+
+		for (WgStatItem currItem : current) {
+			for (WgStatItem lastItem : last) {
+				if (currItem.name().equals(lastItem.name())) {
+					result.add(new WgStatItem(
+							currItem.name(),
+							new BigInteger(currItem.up()).subtract(new BigInteger(lastItem.up())).toString(),
+							new BigInteger(currItem.down()).subtract(new BigInteger(lastItem.down())).toString(),
+							currItem.time()
+					));
+				}
+			}
+		}
+
+		return result;
 	}
 }
---
+++ src/main/java/main/main_tech/wg/WgStatItem.java
@@ -0,0 +1,3 @@
+package main.main_tech.wg;
+
+public record WgStatItem(String name, String up, String down, String time) {}
