a2bbfe0c57ae9230fc4e8b74d3bb04595301b193
ort <orthodoxplus@ya.ru>
2023-08-27 15:18:20 +0300

    pg chat

--- src/main/java/main/pg_chat/Telegram.java
+++ src/main/java/main/pg_chat/Telegram.java
@@ -1,10 +1,14 @@
 package main.pg_chat;
 
+import lombok.extern.slf4j.Slf4j;
 import main.common.telegram.CustomSpringWebhookBot;
 import main.common.telegram.PublicBot;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
+@Slf4j
+@Component
 public class Telegram extends CustomSpringWebhookBot implements PublicBot {
 	@Value("${pg_chat.telegram.channel.id}")
 	private long channelId;
@@ -17,6 +21,15 @@ public class Telegram extends CustomSpringWebhookBot implements PublicBot {
 
 	@Override
 	public void onWebhookUpdate(Update update) {
-
+		if (update.hasMessage()) {
+			if (update.getMessage().getChatId() == chatId) {
+				if (update.getMessage().hasText()) {
+					if (update.getMessage().getText().contains("/report")) {
+						String msg = "https://t.me/pgsql/" + update.getMessage().getMessageId();
+						send(msg(channelId, msg).disableWebPagePreview(true));
+					}
+				}
+			}
+		}
 	}
 }
