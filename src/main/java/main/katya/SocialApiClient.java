6daf323897c517a81c13490c7518d1b6514d2bcc
ort <orthodoxplus@ya.ru>
2023-11-08 01:21:52 +0300

    okhttp to feign

--- src/main/java/main/katya/SocialApiClient.java
+++ src/main/java/main/katya/SocialApiClient.java
@@ -1,17 +1,15 @@
 package main.katya;
 
+import feign.Feign;
+import feign.Request;
 import lombok.extern.slf4j.Slf4j;
 import main.common.HttpClient;
-import okhttp3.Call;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URI;
 import java.util.concurrent.TimeUnit;
 
@@ -20,45 +18,31 @@ import java.util.concurrent.TimeUnit;
 public class SocialApiClient extends HttpClient {
 	@Value("${katya.social.api.url}")
 	private String url;
-	private final OkHttpClient httpClient = baseHttpClient.newBuilder()
-			.callTimeout(2, TimeUnit.MINUTES)
-			.connectTimeout(2, TimeUnit.MINUTES)
-			.readTimeout(2, TimeUnit.MINUTES)
-			.build();
+	private SocialApi client;
 
-	public InputStream getYouTubeFile(URI uri) {
-		Request request = new Request.Builder().get()
-				.url(url + "/youtube/download?url=" + uri)
-				.build();
-		Call call = httpClient.newCall(request);
-		try {
-			Response response = call.execute();
-			InputStream bodyStr = new ByteArrayInputStream(readBinaryBody(response));
-			if (response.isSuccessful()) {
-				return bodyStr;
-			}
-			log.error("http error - 'getYouTubeFile' response code - {}, body - {}", response.code(), bodyStr);
-		} catch (IOException e) {
-			log.error("http error - 'YouTube download'", e);
-		}
-		throw new RuntimeException("error during downloading YouTube file");
+	@PostConstruct
+	private void init() {
+		Request.Options options = new Request.Options(2, TimeUnit.MINUTES, 2, TimeUnit.MINUTES, true);
+		client = Feign.builder()
+				.options(options)
+				.target(SocialApi.class, url);
 	}
 
-	public InputStream getTikTokFile(URI uri) {
-		Request request = new Request.Builder().get()
-				.url(url + "/tiktok/download?url=" + uri)
-				.build();
-		Call call = httpClient.newCall(request);
-		try {
-			Response response = call.execute();
-			InputStream bodyStr = new ByteArrayInputStream(readBinaryBody(response));
-			if (response.isSuccessful()) {
-				return bodyStr;
-			}
-			log.error("http error - 'getTikTokFile' response code - {}, body - {}", response.code(), bodyStr);
+	public ByteArrayInputStream getYouTubeFile(URI uri) {
+		try(feign.Response response = client.youTubeFile(uri.toString())) {
+			return new ByteArrayInputStream(response.body().asInputStream().readAllBytes());
+		} catch (IOException e) {
+			log.error("http error - 'YouTube download'", e);
+			throw new RuntimeException("error during downloading YouTube file");
+		}
+	}
+
+	public ByteArrayInputStream getTikTokFile(URI uri) {
+		try(feign.Response response = client.tikTokFile(uri.toString())) {
+			return new ByteArrayInputStream(response.body().asInputStream().readAllBytes());
 		} catch (IOException e) {
 			log.error("http error - 'tiktok download'", e);
+			throw new RuntimeException("error during downloading tiktok file");
 		}
-		throw new RuntimeException("error during downloading tiktok file");
 	}
 }
