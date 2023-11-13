28cded783a98b0e2cdd7aa1c79f40b3b408ffa35
ort <orthodoxplus@ya.ru>
2023-11-13 21:50:01 +0300

    clean

--- src/main/java/main/habr/Telegram.java
+++ src/main/java/main/habr/Telegram.java
@@ -1,24 +1,33 @@
 package main.habr;
 
-import main.common.telegram.CustomSpringWebhookBot;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.bots.DefaultAbsSender;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+@Slf4j
 @Component
-public class Telegram extends CustomSpringWebhookBot {
-	private final HabrBotConfig config;
+public class Telegram extends DefaultAbsSender {
+	@Value("${habr.telegram.channel_id}")
+	private long channelId;
 
-	public Telegram(HabrBotConfig botConfig) {
-		super(botConfig);
-		config = botConfig;
+	public Telegram(@Value("${habr.telegram.bot.token}") String token) {
+		super(new DefaultBotOptions(), token);
 	}
 
-	@Override
-	public void onWebhookUpdate(Update update) {
-		send("работает");
-	}
-
-	public void sendChannelMessage(String message) {
-		send(msg(config.getChannelId(), message).disableWebPagePreview(true));
+	public void sendChannelMessage(String text) {
+		SendMessage message = SendMessage.builder()
+				.chatId(channelId)
+				.text(text)
+				.disableWebPagePreview(true)
+				.build();
+		try {
+			execute(message);
+		} catch (TelegramApiException e) {
+			log.error("habr send error {}", text, e);
+		}
 	}
 }
