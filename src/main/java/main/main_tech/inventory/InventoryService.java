d9aa5335e2038109d657528dfd2b64e4f7145b5d
ort <orthodoxplus@ya.ru>
2023-11-23 16:18:58 +0300

    join to db

--- src/main/java/main/main_tech/inventory/InventoryService.java
+++ src/main/java/main/main_tech/inventory/InventoryService.java
@@ -2,51 +2,18 @@ package main.main_tech.inventory;
 
 import lombok.RequiredArgsConstructor;
 import main.main_tech.ruvds.api.RuvdsServer;
-import main.main_tech.ruvds.api.ServerMapper;
-import main.regru.RegRuService;
-import main.regru.common.RR;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
-
-import static org.mapstruct.factory.Mappers.getMapper;
 
 @Component
 @RequiredArgsConstructor
 public class InventoryService {
 	private final Repo repo;
-	private final RegRuService regRuService;
-	@Value("${main_tech.regru.domains}")
-	private String[] domains;
 
 	public Set<Server> getServers() {
-		Map<String, String> domainByIpMap = new HashMap<>();
-		for (String domain : domains) {
-			domainByIpMap.putAll(requestDomainsByIpMap(domain));
-		}
-
-		ServerMapper mapper = getMapper(ServerMapper.class);
-
-		return repo.getServers().stream()
-				.map(s -> mapper.map(s, domainByIpMap.get(s.address())))
-				.collect(Collectors.toSet());
-	}
-
-	private Map<String, String> requestDomainsByIpMap(String domain) {
-		return groupDomainByIpAndConcatDomain(regRuService.getSavedSubdomainsList(domain), domain);
-	}
-
-	private Map<String, String> groupDomainByIpAndConcatDomain(List<RR> rrs, String domain) {
-		Map<String, String> result = new HashMap<>();
-		for (RR rr : rrs) {
-			result.put(rr.ip(), "%s.%s".formatted(rr.domain(), domain));
-		}
-		return result;
+		return repo.getServersWithDomains();
 	}
 
 	public void updateServersFromRuvds(Set<RuvdsServer> ruvdsServers) {
--- src/main/java/main/main_tech/inventory/Repo.java
+++ src/main/java/main/main_tech/inventory/Repo.java
@@ -2,6 +2,7 @@ package main.main_tech.inventory;
 
 import lombok.RequiredArgsConstructor;
 import main.main_tech.ruvds.api.RuvdsServer;
+import main.tables.TechInventoryDomainsRecords;
 import main.tables.TechInventoryServers;
 import org.jooq.Condition;
 import org.jooq.DSLContext;
@@ -9,14 +10,17 @@ import org.springframework.stereotype.Component;
 
 import java.util.Set;
 
+import static main.Tables.TECH_INVENTORY_DOMAINS_RECORDS;
 import static main.Tables.TECH_INVENTORY_SERVERS;
 import static org.jooq.Records.mapping;
+import static org.jooq.impl.DSL.concat;
 
 @Component
 @RequiredArgsConstructor
 public class Repo {
 	private final DSLContext db;
 	private final TechInventoryServers tis = TECH_INVENTORY_SERVERS;
+	private final TechInventoryDomainsRecords tidr = TECH_INVENTORY_DOMAINS_RECORDS;
 
 	public void updateServer(RuvdsServer o) {
 		Condition condition = tis.HOSTING_ID.eq(String.valueOf(o.id()))
@@ -35,6 +39,19 @@ public class Repo {
 		for (RuvdsServer ruvdsServer : ruvdsServers) {
 			updateServer(ruvdsServer);
 		}
+	}
+
+	public Set<Server> getServersWithDomains() {
+		return db.select(
+						tis.ID, tis.ADDRESS, tis.SSH_PORT, tis.NAME,
+						concat(concat(tidr.SUB_DOMAIN, "."), tidr.CORE_DOMAIN),
+						tis.CPU, tis.RAM, tis.DRIVE, tis.ADD_DRIVE,
+						tis.HOSTING_ID, tis.OS, tis.ACTIVE_MONITORING,
+						tis.HOSTING_NAME)
+				.from(tis)
+				.leftJoin(tidr)
+					.on(tis.ADDRESS.eq(tidr.ADDRESS))
+				.fetchSet(mapping(Server::new));
 	}
 
 	public Set<ServerDTO> getServers() {
