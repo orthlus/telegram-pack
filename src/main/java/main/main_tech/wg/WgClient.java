e5c5af91d44b71f77921dcb0bac986c263431011
ort <orthodoxplus@ya.ru>
2024-04-03 18:42:46 +0300

    try restclient instead resttemplate

--- src/main/java/main/main_tech/wg/WgClient.java
+++ src/main/java/main/main_tech/wg/WgClient.java
@@ -4,23 +4,33 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class WgClient {
-	@Qualifier("wgRestTemplate")
-	private final RestTemplate client;
+//	@Qualifier("wgRestTemplate")
+//	private final RestTemplate client;
+	@Qualifier("wgRestClient")
+	private final RestClient client2;
 
 	String getRawStat() {
-		return client.getForEntity("/raw-stat", String.class)
-				.getBody();
+		return client2.get()
+				.uri("/raw-stat")
+				.retrieve()
+				.body(String.class);
+//		return client.getForEntity("/raw-stat", String.class).getBody();
 	}
 
 	String getUsers() {
-		return client
-				.getForEntity("/users", String.class)
-				.getBody();
+		return client2.get()
+				.uri("/users")
+				.retrieve()
+				.body(String.class);
+//		return client
+//				.getForEntity("/users", String.class)
+//				.getBody();
 	}
 }
