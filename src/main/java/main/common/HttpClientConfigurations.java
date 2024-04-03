2f2a3dc1e5a26fbe61df6da21fec957160ea40d5
ort <orthodoxplus@ya.ru>
2024-04-03 23:35:27 +0300

    Revert "clean resttemplate"
    This reverts commit 3323a44f4660f7af09a6a2a996ca4ee846eb67ec.

--- src/main/java/main/common/HttpClientConfigurations.java
+++ src/main/java/main/common/HttpClientConfigurations.java
@@ -1,16 +1,30 @@
 package main.common;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.http.client.support.BasicAuthenticationInterceptor;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestTemplate;
 
 import java.time.Duration;
 
 @Configuration
 public class HttpClientConfigurations {
+	@Bean
+	public RestTemplate wgRestTemplate(
+			RestTemplateBuilder builder,
+			@Value("${main_tech.api.url}") String url,
+			@Value("${main_tech.api.user}") String user,
+			@Value("${main_tech.api.secret}") String password) {
+		return builder.basicAuthentication(user, password)
+				.rootUri(url)
+				.setConnectTimeout(Duration.ofMinutes(2))
+				.build();
+	}
+
 	@Bean
 	public RestClient wgRestClient(
 			@Value("${main_tech.api.url}") String url,
--- src/main/java/main/main_tech/wg/WgClient.java
+++ src/main/java/main/main_tech/wg/WgClient.java
@@ -5,11 +5,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestTemplate;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class WgClient {
+//	@Qualifier("wgRestTemplate")
+//	private final RestTemplate client;
 	@Qualifier("wgRestClient")
 	private final RestClient client2;
 
@@ -18,6 +21,7 @@ public class WgClient {
 				.uri("/raw-stat")
 				.retrieve()
 				.body(String.class);
+//		return client.getForEntity("/raw-stat", String.class).getBody();
 	}
 
 	String getUsers() {
@@ -25,5 +29,8 @@ public class WgClient {
 				.uri("/users")
 				.retrieve()
 				.body(String.class);
+//		return client
+//				.getForEntity("/users", String.class)
+//				.getBody();
 	}
 }
