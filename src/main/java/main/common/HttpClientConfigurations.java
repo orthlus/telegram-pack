ca6539d16d5305b1068879be83fad884ccfd8ef5
ort <orthodoxplus@ya.ru>
2024-04-03 23:37:18 +0300

    rollback to resttemplate

--- src/main/java/main/common/HttpClientConfigurations.java
+++ src/main/java/main/common/HttpClientConfigurations.java
@@ -4,9 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
-import org.springframework.http.client.support.BasicAuthenticationInterceptor;
-import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.Duration;
@@ -22,23 +19,6 @@ public class HttpClientConfigurations {
 		return builder.basicAuthentication(user, password)
 				.rootUri(url)
 				.setConnectTimeout(Duration.ofMinutes(2))
-				.build();
-	}
-
-	@Bean
-	public RestClient wgRestClient(
-			@Value("${main_tech.api.url}") String url,
-			@Value("${main_tech.api.user}") String user,
-			@Value("${main_tech.api.secret}") String password
-	) {
-		SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
-		requestFactory.setConnectTimeout(Duration.ofMinutes(2));
-		requestFactory.setReadTimeout(Duration.ofMinutes(2));
-
-		return RestClient.builder()
-				.baseUrl(url)
-				.requestInterceptor(new BasicAuthenticationInterceptor(user, password))
-				.requestFactory(new SimpleClientHttpRequestFactory())
 				.build();
 	}
 }
--- src/main/java/main/main_tech/wg/WgClient.java
+++ src/main/java/main/main_tech/wg/WgClient.java
@@ -4,33 +4,20 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class WgClient {
-//	@Qualifier("wgRestTemplate")
-//	private final RestTemplate client;
-	@Qualifier("wgRestClient")
-	private final RestClient client2;
+	@Qualifier("wgRestTemplate")
+	private final RestTemplate client;
 
 	String getRawStat() {
-		return client2.get()
-				.uri("/raw-stat")
-				.retrieve()
-				.body(String.class);
-//		return client.getForEntity("/raw-stat", String.class).getBody();
+		return client.getForObject("/raw-stat", String.class);
 	}
 
 	String getUsers() {
-		return client2.get()
-				.uri("/users")
-				.retrieve()
-				.body(String.class);
-//		return client
-//				.getForEntity("/users", String.class)
-//				.getBody();
+		return client.getForObject("/users", String.class);
 	}
 }
