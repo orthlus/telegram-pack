f05bc366ff116faffd33dddf6c37e94a93522367
ort <orthodoxplus@ya.ru>
2024-08-31 03:29:58 +0300

    no Qualifier

--- src/main/java/main/billing/instagram/InstagramBalance.java
+++ src/main/java/main/billing/instagram/InstagramBalance.java
@@ -2,19 +2,17 @@ package main.billing.instagram;
 
 import lombok.RequiredArgsConstructor;
 import main.billing.BalanceResponse;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 @Component
 @RequiredArgsConstructor
 public class InstagramBalance implements BalanceResponse {
-	@Qualifier("instagramRestTemplate")
-	private final RestTemplate instagram;
+	private final RestTemplate instagramRestTemplate;
 
 	@Override
 	public String balanceString() {
-		Balance balance = instagram.getForObject("/sys/balance", Balance.class);
+		Balance balance = instagramRestTemplate.getForObject("/sys/balance", Balance.class);
 		return "Баланс: %.4f %s".formatted(balance.getAmount(), balance.getCurrency());
 	}
 
--- src/main/java/main/billing/timeweb/TimewebBalance.java
+++ src/main/java/main/billing/timeweb/TimewebBalance.java
@@ -2,7 +2,6 @@ package main.billing.timeweb;
 
 import lombok.RequiredArgsConstructor;
 import main.billing.BalanceResponse;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,12 +11,11 @@ import java.time.LocalDateTime;
 @Component
 @RequiredArgsConstructor
 public class TimewebBalance implements BalanceResponse {
-	@Qualifier("timewebRestTemplate")
-	private final RestTemplate restTemplate;
+	private final RestTemplate timewebRestTemplate;
 
 	@Override
 	public String balanceString() {
-		FinancesResponse response = restTemplate.getForObject("/api/v1/account/finances", FinancesResponse.class);
+		FinancesResponse response = timewebRestTemplate.getForObject("/api/v1/account/finances", FinancesResponse.class);
 		LocalDate endDate = LocalDateTime.now().plusHours(Integer.parseInt(response.getHoursLeft())).toLocalDate();
 		return """
 				Баланс: %s руб
