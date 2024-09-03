584a1411f41edd78083d6f0050a6dac6b7000278
ort <orthodoxplus@ya.ru>
2024-09-03 13:37:07 +0300

    packages

--- src/main/java/main/config/RestTemplateConfiguration.java
+++ src/main/java/main/config/RestTemplateConfiguration.java
@@ -1,4 +1,4 @@
-package main;
+package main.config;
 
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
--- src/main/java/main/config/TelegramConfig.java
+++ src/main/java/main/config/TelegramConfig.java
@@ -1,4 +1,4 @@
-package main;
+package main.config;
 
 import art.aelaort.SpringLongPollingBot;
 import art.aelaort.TelegramClientBuilder;
