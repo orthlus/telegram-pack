0db2cc6da934ecfa0fe1dcea07430724e8f54abf
ort <orthodoxplus@ya.ru>
2024-09-10 02:07:41 +0300

    list bot

--- src/main/java/main/list/ListBot.java
+++ src/main/java/main/list/ListBot.java
@@ -1,11 +1,13 @@
 package main.list;
 
 import art.aelaort.SpringAdminBot;
+import jakarta.annotation.PostConstruct;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import org.telegram.telegrambots.meta.api.methods.GetMe;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.generics.TelegramClient;
@@ -26,12 +28,19 @@ public class ListBot implements SpringAdminBot {
 	@Getter
 	@Value("${telegram.admin.id}")
 	private long adminId;
+	private String botNickname;
+
+	@PostConstruct
+	private void init() throws Exception {
+		botNickname = listTelegramClient.execute(new GetMe()).getUserName();
+	}
 
 	@Override
 	public void consumeAdmin(Update update) {
 		if (update.hasMessage() && update.getMessage().hasText()) {
 			String[] bots = telegramListRestTemplate.getForObject("/list", String[].class);
 			String text = Stream.of(bots)
+					.filter(bot -> !bot.equals(botNickname))
 					.map(bot -> "@" + bot)
 					.collect(Collectors.joining("\n"));
 
